d36c97032eea171d524dc2fe65b2262e27e1c164
Olivia Cubela <oliviacubela@gmail.com>
2020-06-16 19:57:09 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -52,20 +52,20 @@ def monopoly_with_third_tier
 			:pieces=>4,
 			:names=>
 			{
-			# :reading_railroad=>
-			# {
+			:reading_railroad=>
+			{
 			#   "mortgage_value"=>"$100"
-			# },
-			# :pennsylvania_railroad=>
-			# {
+			},
+			:pennsylvania_railroad=>
+			{
 			#   "mortgage_value"=>"$200"
-			# },
-			# :b_and_o_railroad=>
-			# {
+			},
+			:b_and_o_railroad=>
+			{
 			#   "mortgage_value"=>"$400"
-			# },
-			# :shortline=>
-			# {
+			},
+			:shortline=>
+			{
 			#   "mortgage_value"=>"$800"
 			},
 			:rent_in_dollars=>
