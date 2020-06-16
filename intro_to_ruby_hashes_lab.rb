9f168536799d7d62c9d1c23926aa18ddbe8ec9e2
Olivia Cubela <oliviacubela@gmail.com>
2020-06-16 19:53:11 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,9 +16,14 @@ def monopoly_with_second_tier
 		:railroads=>
 		{
 			:pieces=>4,
-			:name::new,
-			:rent_in_dollars=> (Hash.new)
-
+			:names=>,
+			:rent_in_dollars=>
+	    {
+	      # :one_piece_owned=>25,
+	      # :two_pieces_owned=>50,
+	      # :three_pieces_owned=>100,
+	      # :four_pieces_owned=>200
+	    }
 		}
 	}
 end
